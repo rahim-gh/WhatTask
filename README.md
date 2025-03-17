d820b7d82bb522d5f5ada72f54cc0a3fd9e89afc
rahim-gh <rahimguerrouf0@gmail.com>
2025-03-17 12:10:20 +0100

    Push the initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# what_task
+
+What Task? a task manager to remind you, to annoy you. until it's done.
---
+++ android/app/src/main/kotlin/app/what_task/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.what_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
